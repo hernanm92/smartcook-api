86dedf139b0af81e6b5ee4f3f4a2f07e22b6102e
Hernan Maschwitz <hernanm992@gmail.com>
2016-08-06 20:23:06 -0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :name, presence: true, length: { in: 5..30 }
-  validates :email, uniqueness: true, presence: true, length: { in: 5..30 }
+  validates :email, presence: true, uniqueness: true, length: { in: 5..30 }
   validates :password, presence: true, length: { in: 5..30 }
+
+  after_initialize :set_defaults
 
   def self.find_by!(params)
     params[:username] = params.delete(:id) if params[:id]
     super params
+  end
+
+  private
+
+  def set_defaults
+    self.vegetarian = false if vegetarian.nil?
+    self.vegan = false if vegan.nil?
+    self.celiac = false if celiac.nil?
+    self.diabetic = false if diabetic.nil?
   end
 end
