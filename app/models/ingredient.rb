1b1c684abe8b8bed065313ad459bbf0c17dcfb7d
Hernan Maschwitz <hernanm992@gmail.com>
2016-08-23 22:14:17 -0300

    remove boolean validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -24,10 +24,10 @@ class Ingredient < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true, length: { in: 3..30 }
   validates :image_url, presence: true, uniqueness: true
   validates :food_category_id, presence: true, numericality: { only_integer: true }
-  validates :vegetarian, presence: true, inclusion: { in: [true, false] }
-  validates :vegan, presence: true, inclusion: { in: [true, false] }
-  validates :celiac, presence: true, inclusion: { in: [true, false] }
-  validates :diabetic, presence: true, inclusion: { in: [true, false] }
+  validates :vegetarian, inclusion: { in: [true, false] }
+  validates :vegan, inclusion: { in: [true, false] }
+  validates :celiac, inclusion: { in: [true, false] }
+  validates :diabetic, inclusion: { in: [true, false] }
   validates :calories, presence: true, numericality: true
   validates :proteins, presence: true, numericality: true
   validates :carbohydrates, presence: true, numericality: true
