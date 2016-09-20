ac0e15fb19a8344e5daa7918ea96136c1658e234
matiasleon <matias.leon.peralta@gmail.com>
2016-09-20 20:53:21 -0300

    get user by  username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.find_by_username(params[:username]) if params[:username]
+    @users = User.all unless @users
     render json: @users
   end
 
